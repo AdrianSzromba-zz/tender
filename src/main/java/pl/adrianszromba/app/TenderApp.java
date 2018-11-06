bb224c4917fdc6ba08f2882e6490c192efd9f30f
AdrianSzromba <adrian.szromba@gmail.com>
2018-11-06 19:28:36 +0100

    set Spring boot app main class

--- src/main/java/pl/adrianszromba/app/TenderApp.java
+++ src/main/java/pl/adrianszromba/app/TenderApp.java
@@ -1,8 +1,12 @@
 package pl.adrianszromba.app;
 
-public class TenderApp {
-	
-	public static void main(String[] args) {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class TenderApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenderApp.class, args);
 	}
 }
