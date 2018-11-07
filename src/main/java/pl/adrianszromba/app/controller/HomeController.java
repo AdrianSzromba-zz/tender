d408719db920cedf4685cc880112892b6c726e31
AdrianSzromba <adrian.szromba@gmail.com>
2018-11-07 21:45:50 +0100

    Root controller

---
+++ src/main/java/pl/adrianszromba/app/controller/HomeController.java
@@ -0,0 +1,13 @@
+package pl.adrianszromba.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "home page";
+	}
+}
