c4401d14e0e2ede200dc5bea05640eb4a864d0ce
AdrianSzromba <adrian.szromba@gmail.com>
2018-11-08 22:36:02 +0100

    user test structure

--- src/main/java/pl/adrianszromba/app/entity/UserEntity.java
+++ src/main/java/pl/adrianszromba/app/entity/UserEntity.java
@@ -1,4 +1,48 @@
 package pl.adrianszromba.app.entity;
 
 public class UserEntity {
+
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String login;
+
+	public UserEntity(String firstName, String lastName, int age, String login) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.login = login;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
 }
