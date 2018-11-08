57ffaffb7744cbb4c70f17b0070ec4cecb583dc9
AdrianSzromba <adrian.szromba@gmail.com>
2018-11-08 23:16:59 +0100

    Autowired homeService

--- src/main/java/pl/adrianszromba/app/controller/HomeController.java
+++ src/main/java/pl/adrianszromba/app/controller/HomeController.java
@@ -1,10 +1,19 @@
 package pl.adrianszromba.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.adrianszromba.app.service.HomeService;
 
 @RestController
 public class HomeController {
+
+	private final HomeService homeService;
+
+	@Autowired
+	public HomeController(HomeService homeService) {
+		this.homeService = homeService;
+	}
 
 	@RequestMapping("/")
 	public String home() {
