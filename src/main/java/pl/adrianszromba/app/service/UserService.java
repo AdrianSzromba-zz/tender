1e9c856ac7ecec0851777eba0ef4bc323c394ea2
AdrianSzromba <adrian.szromba@gmail.com>
2018-11-10 15:33:23 +0100

    change users to mutable list

--- src/main/java/pl/adrianszromba/app/service/UserService.java
+++ src/main/java/pl/adrianszromba/app/service/UserService.java
@@ -3,16 +3,17 @@ package pl.adrianszromba.app.service;
 import org.springframework.stereotype.Service;
 import pl.adrianszromba.app.entity.User;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class UserService {
 
-	private List<User> users = Arrays.asList(new User("James", "Cameron", 30, "Jamie"),
+	private List<User> users = new ArrayList<>(Arrays.asList(new User("James", "Cameron", 30, "Jamie"),
 			new User("Colin", "Morta", 30, "Mortadela"),
 			new User("Freddy", "Num", 30, "Numie")
-	);
+	));
 
 	public List<User> getAllUsers () {
 		return users;
@@ -20,5 +21,9 @@ public class UserService {
 
 	public User getUserByLogin (String login) {
 		return users.stream().filter(u -> u.getLogin().equals(login)).findFirst().get();
+	}
+
+	public void addUser(User user) {
+		users.add(user);
 	}
 }
